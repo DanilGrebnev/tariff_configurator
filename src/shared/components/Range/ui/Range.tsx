bbc6a33f31686c15284b0a8830d6b07699419318
Danil <grenevdanil60@gmail.com>
2023-10-30 10:31:03 +0300

    add: range zone

--- src/shared/components/Range/ui/Range.tsx
+++ src/shared/components/Range/ui/Range.tsx
@@ -1,23 +1,15 @@
-import {
-    FC,
-    MouseEventHandler,
-    memo,
-    useMemo,
-    useState,
-    useRef,
-    useEffect,
-    useCallback,
-} from 'react'
+import { FC, memo, useState, useRef, useEffect, useCallback } from 'react'
 import Rectangle from 'shared/assets/icons/reactangle.svg?react'
-import { debounce } from 'shared/lib/debounce'
 import cn from 'classnames'
 import s from './Range.module.scss'
+import { debounce } from '@/shared/lib/debounce'
 
 interface RangeProps {
     className?: string
 }
 
 type TRangeLine = number | undefined
+type TRangeZones = { [key: string]: number }
 
 export const Range: FC<RangeProps> = memo(() => {
     const rangeLineRef = useRef<HTMLDivElement>(null)
@@ -25,6 +17,7 @@ export const Range: FC<RangeProps> = memo(() => {
     const rangeContainerRef = useRef<HTMLDivElement>(null)
     const thumbRef = useRef<HTMLDivElement>(null)
 
+    //Длинна range контейнера
     const [rangeContainerWidth, setRangeContainerWidth] = useState<TRangeLine>(0)
 
     // Получаем начало range контейнера относительно страницы
@@ -36,6 +29,10 @@ export const Range: FC<RangeProps> = memo(() => {
 
     //Считываем позицию курсора мыши
     const [cursorPositionX, setCursorPositionX] = useState(0)
+
+    const [rangeZones, setRangeZones] = useState<TRangeZones>({})
+
+    const [value, setValue] = useState('')
 
     const changeThumbPosition = useCallback(
         ({ clientX }: MouseEvent) => {
@@ -96,14 +93,66 @@ export const Range: FC<RangeProps> = memo(() => {
     useEffect(() => {
         // Узнаем, на каком расстоянии от левого края экрана находится range
         const rangeContainer = rangeContainerRef.current
-        const clientX = rangeContainer?.getBoundingClientRect().x
-        setRangeContainerStartPosition(clientX)
+        const rangeContainerStartCoordinateX = rangeContainer?.getBoundingClientRect().x
+        setRangeContainerStartPosition(rangeContainerStartCoordinateX)
         // Устанавливаем длинну контейнера
         setRangeContainerWidth(rangeContainer?.clientWidth)
     }, [])
 
-    const rangeFillLineWidthPX = cursorPositionX + 'px'
+    /** Функция
+     *
+     */
+    const calculateRangeZone = useCallback(
+        (rangeContainerWidth: number) => {
+            const zoneWidth = rangeContainerWidth / 4
+            const o: { [key: string]: number } = {}
 
+            for (let i = 0; i < 4; i++) {
+                if (i === 0) {
+                    o['zone' + i] = zoneWidth
+                    continue
+                }
+
+                o['zone' + i] = o['zone' + (i - 1)] + zoneWidth
+            }
+            // Прибавляем к значениям позицию с которой начинается range контейнер
+            Object.entries(o).forEach(([k, v]) => (o[k] = v + rangeContainerStartPosition!))
+
+            setRangeZones(rangeZones)
+        },
+        [rangeContainerStartPosition, rangeZones]
+    )
+
+    useEffect(() => {
+        if (!rangeContainerWidth) return
+        calculateRangeZone(rangeContainerWidth)
+    }, [rangeContainerWidth, calculateRangeZone])
+
+    /**
+     * Функция считает значение относительно положения
+     * thumb
+     */
+    const calculateThumbValues = useCallback(() => {
+        const a = Object.entries(rangeZones)
+
+        for (let i = 0; i < a.length; i++) {
+            const [zoneName, zoneCoordinateEnd] = a[i]
+            /**  Если курсор по координате x находится
+            ближе к левому краю экрана, чем заканчивается
+            зона значения, тогда считается, текущая зона
+            */
+            if (cursorPositionX <= zoneCoordinateEnd) {
+                console.log(zoneName)
+                break
+            }
+        }
+    }, [cursorPositionX, rangeZones])
+
+    useEffect(() => {
+        calculateThumbValues()
+    }, [calculateThumbValues])
+
+    const rangeFillLineWidthPX = cursorPositionX + 'px'
     const thumbCenter = (thumbRef.current?.clientHeight as number) / 2
     const thumbXPositionPX = cursorPositionX - thumbCenter + 'px'
 
